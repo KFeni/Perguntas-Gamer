72cbec6af9e4e8441458f74a14e572939ae1be23
KFeni <169073394+KFeni@users.noreply.github.com>
2024-10-20 22:00:40 -0300

    Alura aula

---
+++ perguntas.js
@@ -0,0 +1,55 @@
+criaCartao (
+   'Minecraft',
+   'Quantos minerios tem no jogo?',
+   'No jogo tem 11 minerios'
+)
+
+criaCartao(
+    'Mundo Gamer',
+    'Qaundo foi lançado o primeiro God of War?',
+    'Foi lançado em 2005'
+)
+
+ criaCartao(
+    'Mundo Gamer',
+    'Em que ano foi lançado o PS3?',
+    'O PS3 foi lançado em 2006'
+ )
+
+ criaCartao(
+    'Mundo Gamer',
+    'Qual foi o primeiro jogo do mundo?',
+    'Foi o jogo Tennis for Two'
+ )
+
+ criaCartao(
+    'Youtube',
+    'Qual é o youtube mais jegue?',
+    'O youtube mais jegue é o Jazzghost'
+ )
+
+ criaCartao(
+    'Anime',
+    'Qual foi Gear do Luffy para derrotar o Rob lucci?',
+    'Foi o Gear Second'
+ )
+
+ criaCartao(
+    'Anime',
+    'Qual Akuma no mi é do Rob Lucci?',
+    'é a Neko Neko no mi'
+ )
+
+ criaCartao(
+    'Minecraft',
+    'Qual o mob que tem mais vida?',
+    'O mob é o Wither'
+ )
+
+ criaCartao(
+    'Youtube',
+    'Qual é o youtube consegui 1 trilhão de visualizações?',
+    'Foi o youtube "Minecraft"'
+ )
+
+
